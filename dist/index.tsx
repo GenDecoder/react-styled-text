60c93a41bcad9943d6fd7cd5f9a28cf54a4d6fe0
Mario <mario.medrano.maldonado@gmail.com>
2020-06-07 21:46:25 -0400

    Reduce number of object used

--- dist/index.tsx
+++ dist/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
 import './styles.css';
 import { TStyledTextProps } from './types';
 import { getSpace } from './utils';
@@ -27,37 +27,38 @@ const StyledText = ({
     ...others
 }: TStyledTextProps) => {
     const TextTag = `${tag}` as keyof JSX.IntrinsicElements;
+    let customStyles: CSSProperties = {};
+
+    bold && (customStyles.fontWeight = 'bold');
+    center && (customStyles.textAlign = 'center');
+    color && (customStyles.color = color);
+    fontFamily && (customStyles.fontFamily = fontFamily);
+    fontSize && (customStyles.fontSize = fontSize);
+    highlighColor && (customStyles.backgroundColor = highlighColor);
+    letterSpacing && (customStyles.letterSpacing = letterSpacing);
+    lineSpacing && (customStyles.lineHeight = lineSpacing);
+    marginTop && (customStyles.marginTop = marginTop);
+    uppercase && (customStyles.textTransform = 'uppercase');
 
     return (
         <TextTag
             className={`styled-text ${className || ''}`}
             style={{
-                ...{
-                    ...(bold && { fontWeight: 'bold' }),
-                    ...(center && { textAlign: 'center' }),
-                    ...(color && { color }),
-                    ...(fontFamily && { fontFamily }),
-                    ...(fontSize && { fontSize }),
-                    ...(highlighColor && { backgroundColor: highlighColor }),
-                    ...(letterSpacing && { letterSpacing }),
-                    ...(lineSpacing && { lineHeight: lineSpacing }),
-                    ...(marginTop && { marginTop }),
-                    ...(uppercase && { textTransform: 'uppercase' }),
-                    ...(numberOfLines && {
-                        display: '-webkit-box',
-                        WebkitLineClamp: numberOfLines,
-                        WebkitBoxOrient: 'vertical',
-                        overflow: 'hidden',
-                    }),
-                    ...(!selectable && {
-                        WebkitTouchCallout: 'none' /* iOS Safari */,
-                        WebkitUserSelect: 'none' /* Safari */,
-                        MozUserSelect: 'none' /* Old versions of Firefox */,
-                        MsUserSelect: 'none' /* Internet Explorer/Edge */,
-                        userSelect:
-                            'none' /* Non-prefixed version, currently supported by Chrome, Edge, Opera and Firefox */,
-                    }),
-                },
+                ...customStyles,
+                ...(numberOfLines && {
+                    display: '-webkit-box',
+                    WebkitLineClamp: numberOfLines,
+                    WebkitBoxOrient: 'vertical',
+                    overflow: 'hidden',
+                }),
+                ...(!selectable && {
+                    WebkitTouchCallout: 'none' /* iOS Safari */,
+                    WebkitUserSelect: 'none' /* Safari */,
+                    MozUserSelect: 'none' /* Old versions of Firefox */,
+                    MsUserSelect: 'none' /* Internet Explorer/Edge */,
+                    userSelect:
+                        'none' /* Non-prefixed version, currently supported by Chrome, Edge, Opera and Firefox */,
+                }),
                 ...style,
             }}
             {...others}
