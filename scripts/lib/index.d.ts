cb25eb4b517d54f694e3db4e8326a92a14d76eb1
Mario <mario.medrano.maldonado@gmail.com>
2020-06-09 00:39:21 -0400

    Update tsconfig

--- scripts/lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="react" />
-import './styles.css';
-import { TStyledTextProps } from './types';
-declare const StyledText: ({ bold, center, children, className, color, fontFamily, fontSize, hasLeftSpace, hasRightSpace, highlighColor, indent, letterSpacing, lineSpacing, marginTop, numberOfLines, selectable, style, tabSize, tag, uppercase, ...others }: TStyledTextProps) => JSX.Element;
-export default StyledText;
